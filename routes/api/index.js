ceaf8e2673096e6bcff29e06f8d85362f3538fc8
Chris Waters <floridarealty001@gmail.com>
2022-04-21 15:00:24 -0400

    new router lines

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-const thoughtRoutes = require('./thought-routes');
-const userRoutes = require('./user-routes');
 
-router.use('/thoughts', thoughtRoutes);
-router.use('/users', userRoutes);
+const thoughtsRoute = require('./thought-routes');
+const usersRoutes = require('./user-routes');
+
+router.use('/thoughts', thoughtsRoute);
+router.use('/users', usersRoutes);
 
 module.exports = router;
