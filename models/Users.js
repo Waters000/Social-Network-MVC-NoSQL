b6768e2a384a6e495de1d1b3062011f22af04b08
Chris Waters <floridarealty001@gmail.com>
2022-04-21 13:48:06 -0400

    user model and api routes started

---
+++ models/Users.js
@@ -0,0 +1,49 @@
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
+
+const usersSchema = new Schema(
+    {
+  userName: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: [validateEmail, "Please fill a valid email address"],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please fill a valid email address",
+    ],
+  },
+  thoughts: [
+      {
+          type: Schema.Types.ObjectId,
+          ref: "Thoughts"
+      }
+  ],
+  
+  friends: [
+      {
+          type: Schema.Types.ObjectId,
+          ref: "Users"
+      }
+  ],
+},
+{
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+}
+);
+
+usersSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+})
+
+const Users = model('Users', usersSchema)
